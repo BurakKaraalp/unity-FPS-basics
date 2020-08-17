dd8f9da7bce2e64d4a22474d778cbb181165ef3f
BurakKaraalp <61118825+BurakKaraalp@users.noreply.github.com>
2020-08-17 14:41:29 +0300

    Update MouseLook.cs

--- MouseLook.cs
+++ MouseLook.cs
@@ -21,7 +21,7 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X")*sensivity*Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y")*sensivity*Time.deltaTime;
         otherRotation -= mouseY;
-        otherRotation = Mathf.Clamp(xrotation, -90f, 90f);
+        otherRotation = Mathf.Clamp(otherRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(otherRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
