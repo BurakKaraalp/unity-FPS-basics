17019f4c0b444cea64a5ce3b0308d86f59ff309c
BurakKaraalp <61118825+BurakKaraalp@users.noreply.github.com>
2020-08-17 14:27:10 +0300

    Create MouseLook.cs

---
+++ MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensivity;
+    float xrotation = 0f;
+    public Transform playerBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*sensivity*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")*sensivity*Time.deltaTime;
+        xrotation -= mouseY;
+        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    
+}
