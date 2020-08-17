0313ed00f0d21ccdb8e5ac4fc7d4af2be87bd3a0
BurakKaraalp <61118825+BurakKaraalp@users.noreply.github.com>
2020-08-17 14:27:06 +0300

    Create MovementScript.cs

---
+++ MovementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float Speed = 10f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move*Speed*Time.deltaTime);
+
+    }
+}
